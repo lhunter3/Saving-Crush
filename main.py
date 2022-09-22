c21729aa4a01b4d2d9e59a90fa09253d097727d9
Lucas Hunter <80433013+lhunter3@users.noreply.github.com>
2022-09-22 10:00:51 -0300

    updates to menus

--- main.py
+++ main.py
@@ -1,37 +1,34 @@
 import pygame, sys, time, random, colorsys, math
 from pygame.math import Vector2
 from pygame.locals import *
-from background import Background
+from button import Button 
 
 
 def main():
     pygame.init()
 
-
     #FPS
     clock = pygame.time.Clock()
     FPS = 60
+    clock.tick(FPS)
 
     # set the display
     DISPLAY=pygame.display.set_mode((640,480),0,32)
     pygame.display.set_caption('Save the Turtles')
-    lsBg = pygame.image.load("data/gfx/lsBg.png").convert()
+
+    # loading screen bg
+
+    lsBg = pygame.image.load("data/gfx/lsBg2.jpeg")
+    lsBg = pygame.transform.smoothscale(lsBg, (640,480))
+
     lsBgHeight = lsBg.get_height()
     lsBgWidth = lsBg.get_width()
     lsBgRec = lsBg.get_rect()
-    
     tiles = math.ceil(480/lsBgHeight) +1
     
     
     # get fonts
     font_small = pygame.font.Font('data/fonts/font.otf', 32)
-
-    # get some images
-    
-
-    # get sounds
-    
-
 
     # colors
     BLUE=(29, 162, 216)
@@ -45,28 +42,26 @@ def main():
     last_time = time.time()
     loadingScreenScroll = 0
     loadingScreenScrollX = 0
+
     # creating a new object player
 
    
-
     
+    menu = True
 
     while loadingScreenTimer < 100:
 
-        clock.tick(FPS)
+        
         dt = time.time() - last_time
         dt *= 60
         last_time = time.time()
         loadingScreenTimer += dt
 
-        startMessage = font_small.render("LOCATING TURTLES", True, WHITE)
-        
-
+        startMessage = font_small.render("LOCATING TURTLES...", True, GREENBLUE)
         # loadingScreen background
-        for i in range(0, tiles):
-            DISPLAY.blit(lsBg, (0.2*loadingScreenScrollX,i * lsBgHeight + loadingScreenScroll))
+        DISPLAY.fill(DARKBLUE)
 
-        loadingScreenScroll -= 3
+        loadingScreenScroll -= 1
         loadingScreenScrollX += 3
                         
         #reset scroll
@@ -90,8 +85,61 @@ def main():
 
 
 
+    while menu:
+
+        # TITLE 
+        DISPLAY.blit(lsBg, (0,0))
+
+        title = font_small.render("SAVE THE TURTLES",True, (0,0,0))
+        DISPLAY.blit(title, (DISPLAY.get_width()/2 - title.get_width()/2, DISPLAY.get_height()/6 - title.get_height()/2))  
+
+        ## MENU BUTTONS
+        start_img = pygame.image.load("data/gfx/start_btn.png").convert()
+        exit_img = pygame.image.load("data/gfx/exit_btn.png").convert()
+        start_button = Button(DISPLAY.get_width()/4, 200, start_img, 0.4)
+        exit_button = Button(DISPLAY.get_width()/2 , 200, exit_img, 0.4)
+        
+        ## START
+        if start_button.draw(DISPLAY):
+            print("START")
+            menu = False
+            game = True
+            break
+
+        ## EXIT
+        if exit_button.draw(DISPLAY):
+            pygame.quit()
+            sys.exit()
 
         
+        ## EXIT WINDOW
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        
+        # update display
+        pygame.display.update()
+
+        
+
+    while game:
+
+        #1. Scrolling bg
+        #2. Loading Turtle Sprites.
+        #3. Load Player
+        #4. Turtle save 
+        # UI
+
+        DISPLAY.fill(WHITE)
+   
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        
+        # update display
+        pygame.display.update()
 
 
 
